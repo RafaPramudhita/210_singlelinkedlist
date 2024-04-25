1f51b8dd716115b56ef78912a9cc46adc911518d
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-04-25 14:18:59 +0700

    membuat node baru pada di dalam node

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -9,6 +9,16 @@ struct Node {
 
 Node* START = NULL;
 void addNode() {
-   
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukan NIM: ";
+    cin >> nim;
+    cout << "Masukan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+
+
 }
 
