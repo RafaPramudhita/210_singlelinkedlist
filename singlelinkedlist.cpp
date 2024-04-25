04e208e4838c4d67a1dde15427511a4558dab302
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-04-25 14:51:20 +0700

    membuat case 2 memeriksa daftar pada listempty

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -150,7 +150,23 @@ int main() {
                 system("pause");
                 system("cls");
                 break;
-            
+            case 2:
+                if (listEmpty())
+                {
+                    cout << "List Kosong" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                }
+                int nim;
+                cout << "Masukan NIM: ";
+                cin >> nim;
+                if (deleteNode(nim)) {
+                    cout << "nim: " << nim << " berhasi dihapus" << endl;
+                    system("pause");
+                    system("cls");
+                }
+           
             }
         }
         catch (exception e)
